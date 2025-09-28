6bc0880726ff37d67652e8d9ed1d30247b057467
Chirag Ailani <chiragailani35@gmail.com>
2025-09-28 19:07:11 +0530

    new file

---
+++ generate_key.py
@@ -0,0 +1,14 @@
+from kiteconnect import KiteConnect
+
+kite = KiteConnect(api_key="your_api_key")
+
+# Redirect the user to the login url obtained
+# from kite.login_url(), and receive the request_token
+# from the registered redirect url after the login flow.
+# Once you have the request_token, obtain the access_token
+# as follows.
+
+kite.login_url()
+
+data = kite.generate_session("request_token_here", api_secret="your_secret")
+kite.set_access_token(data["access_token"])
